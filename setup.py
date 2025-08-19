50ccd5b2b578144ee2194314df80cc4d1d3f6675
Rajat <tech@remotephysios.com>
2025-08-19 10:39:46 +0530

    req add

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Rag_chatbot',
+    version='0.1',
+    author='Rajat Thakur',
+    packages=find_packages(),
+    install_requires=[]
+)
